3ef187bb3a076797c570df10b970aaf2398b05f5
Mojitokyk <dbsrms90@naver.com>
2023-08-18 14:18:58 +0900

    Initial commit

---
+++ src/main/java/kr/or/iei/MungMungProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.or.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungMungProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MungMungProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/or/iei/MungMungProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.iei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MungMungProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
