411622efb6e8e86ee15e71fc85b33e100dd84989
kyungsik1101 <142005877+kyungsik1101@users.noreply.github.com>
2023-08-22 11:01:37 +0900

    로그인 커밋2

--- src/main/java/kr/or/iei/member/model/dao/MemberDao.java
+++ src/main/java/kr/or/iei/member/model/dao/MemberDao.java
@@ -1,5 +1,29 @@
 package kr.or.iei.member.model.dao;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import kr.or.iei.member.model.vo.Member;
+import kr.or.iei.member.model.vo.MemberRowMapper;
+
+@Repository
 public class MemberDao {
+
+	@Autowired
+	private JdbcTemplate jdbc;
+	@Autowired
+	private MemberRowMapper memberRowMapper;
+	
+	public Member selectOneMember(String signId, String signPw) {
+		String query = "select * from member where member_id = ? and member_pw = ?";
+		List list = jdbc.query(query, memberRowMapper, signId, signPw);
+		if(list.isEmpty()) {
+			return null;
+		}
+		return (Member)list.get(0);
+	}
 
 }
