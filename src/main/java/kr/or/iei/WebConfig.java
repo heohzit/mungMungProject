752d36ee493da4d2a86b511c5401af51ee357824
heohzit <heohzit@gmail.com>
2023-08-24 10:00:51 +0900

    어제 커밋 못함요;

--- src/main/java/kr/or/iei/WebConfig.java
+++ src/main/java/kr/or/iei/WebConfig.java
@@ -15,11 +15,15 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("/**")
 			.addResourceLocations("classpath:templates/","classpath:/static/");
 		
+		registry.addResourceHandler("/boardmain/**")
+		.addResourceLocations("file:///C:/Temp/upload/boardmain/");
+		
 		registry.addResourceHandler("/board/**")
 		.addResourceLocations("file:///C:/Temp/upload/board/");
+		
+		registry.addResourceHandler("/notice/**")
+		.addResourceLocations("file:///C:/Temp/upload/notice/");
 
-		registry.addResourceHandler("/editor/**")
-		.addResourceLocations("file:///C:/Temp/upload/editor/");
 	}
 
 
--- src/main/java/kr/or/iei/board/model/service/BoardService.java
+++ src/main/java/kr/or/iei/board/model/service/BoardService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import kr.or.iei.board.model.dao.BoardDao;
 import kr.or.iei.board.model.vo.Board;
-import kr.or.iei.board.model.vo.BoardListData;
+import kr.or.iei.board.model.vo.BoardComment;
+import kr.or.iei.board.model.vo.BoardViewData;
+import kr.or.iei.notice.model.vo.Notice;
 
 @Service
 public class BoardService {
@@ -33,16 +35,54 @@ public class BoardService {
 		return boardList;
 	}
 	
-	//커뮤니티 보기
-	public Board selectOneNotice(int boardNo) {
+	//커뮤니티 상세보기
+	public BoardViewData selectOneNotice(int boardNo) {
 		//공지사항 조회수
 		int count = boardDao.updateReadCount(boardNo);
 		if(count>0) {
-			Board b = boardDao.selectOneNotice(boardNo);	
-			return b;
+			//커뮤니티 상세보기
+			Board b = boardDao.selectOneNotice(boardNo);
+			//커뮤니티 댓글 조회
+			List commentList = boardDao.selectCommentList(boardNo);
+			//커뮤니티 대댓글 조회
+			List reCommentList = boardDao.selectReCommentList(boardNo);
+			BoardViewData bvd = new BoardViewData(b,commentList,reCommentList);
+			return bvd;
 		}else {
 			return null;
 		}
+	}
+	
+	//커뮤니티 삭제
+	public int deleteBoard(int boardNo) {
+		int result = boardDao.deleteNotice(boardNo);
+		if(result > 0) {
+			return result;
+		}else {
+			return 0;
+		}
+	}
+
+	//커뮤니티 해당번호 가져오기
+	public Board getBoard(int boardNo) {
+		Board b = boardDao.selectOneNotice(boardNo);
+		return b;
+	}
+	
+	//커뮤니티 수정
+	public int updateBoard(Board b) {
+		int result = boardDao.updateBoard(b);
+		if(result > 0) {
+			return result;
+		}else {
+			return 0;
+		}
+	}
+	
+	//커뮤니티 댓글쓰기
+	public int insertComment(BoardComment bc) {
+		int result = boardDao.insertComment(bc);
+		return result;
 	}
 
 }
