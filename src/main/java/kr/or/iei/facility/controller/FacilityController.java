df95ed10e69e027f569f50686620c2d6e2b1c305
YeonJHw <142170249+YeonJHw@users.noreply.github.com>
2023-08-25 14:56:34 +0900

    update list link(->detail)

--- src/main/java/kr/or/iei/facility/controller/FacilityController.java
+++ src/main/java/kr/or/iei/facility/controller/FacilityController.java
@@ -1,10 +1,9 @@
 package kr.or.iei.facility.controller;
 
 
-import java.util.List;
 import java.io.File;
 import java.io.IOException;
-
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
